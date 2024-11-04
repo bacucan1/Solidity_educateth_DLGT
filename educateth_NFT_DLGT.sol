bb236c6240d5580fb5c11f49bbb446e9af615b6e
bacucan1 <dlgt1606@gmail.com>
2024-11-03 21:35:19 -0500

    Actividad nft 3.0

--- educateth_NFT_DLGT.sol
+++ educateth_NFT_DLGT.sol
@@ -1,51 +1,21 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.22;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DLGT_NFT is ERC721, ERC721Enumerable, Ownable {
-    uint256 private _nextTokenId;
+contract CypherpunkNFTV4 is ERC721URIStorage, Ownable {
+    uint256 public tokenCounter;
 
-    constructor(address initialOwner)
-        ERC721("DLGT_NFT", "DLGT")
-        Ownable(initialOwner)
-    {}
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://gateway.lighthouse.storage/ipfs/bafkreicegycumgqv3o35eb5yaghfyg3aekip4444zzqudkhzynajpzp3ji";
+    constructor(address initialOwner) ERC721("Educateth_DLGT_3.0", "DG") Ownable(initialOwner) {
+        tokenCounter = 0;
     }
 
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _update(address to, uint256 tokenId, address auth)
-        internal
-        override(ERC721, ERC721Enumerable)
-        returns (address)
-    {
-        return super._update(to, tokenId, auth);
-    }
-
-    function _increaseBalance(address account, uint128 value)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._increaseBalance(account, value);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+    function createNFT() public onlyOwner returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, "https://gateway.lighthouse.storage/ipfs/bafkreiees63v6u2uaibhaqy6el2nvagmkfnezniyaqm2z5aseeoqdsi63y");
+        tokenCounter += 1;
+        return newTokenId;
     }
 }
--- educateth_NFT_DLGT_flattened.sol
+++ educateth_NFT_DLGT_flattened.sol
@@ -3058,210 +3058,104 @@ abstract contract ERC721 is Context, ERC165, IERC721, IERC721Metadata, IERC721Er
     }
 }
 
-// File: @openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol
+// File: @openzeppelin/contracts/interfaces/IERC165.sol
 
 
-// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC721/extensions/IERC721Enumerable.sol)
+// OpenZeppelin Contracts (last updated v5.0.0) (interfaces/IERC165.sol)
 
 pragma solidity ^0.8.20;
 
 
-/**
- * @title ERC-721 Non-Fungible Token Standard, optional enumeration extension
- * @dev See https://eips.ethereum.org/EIPS/eip-721
- */
-interface IERC721Enumerable is IERC721 {
-    /**
-     * @dev Returns the total amount of tokens stored by the contract.
-     */
-    function totalSupply() external view returns (uint256);
+// File: @openzeppelin/contracts/interfaces/IERC721.sol
 
-    /**
-     * @dev Returns a token ID owned by `owner` at a given `index` of its token list.
-     * Use along with {balanceOf} to enumerate all of ``owner``'s tokens.
-     */
-    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
 
-    /**
-     * @dev Returns a token ID at a given `index` of all the tokens stored by the contract.
-     * Use along with {totalSupply} to enumerate all tokens.
-     */
-    function tokenByIndex(uint256 index) external view returns (uint256);
+// OpenZeppelin Contracts (last updated v5.0.0) (interfaces/IERC721.sol)
+
+pragma solidity ^0.8.20;
+
+
+// File: @openzeppelin/contracts/interfaces/IERC4906.sol
+
+
+// OpenZeppelin Contracts (last updated v5.1.0) (interfaces/IERC4906.sol)
+
+pragma solidity ^0.8.20;
+
+
+
+/// @title ERC-721 Metadata Update Extension
+interface IERC4906 is IERC165, IERC721 {
+    /// @dev This event emits when the metadata of a token is changed.
+    /// So that the third-party platforms such as NFT market could
+    /// timely update the images and related attributes of the NFT.
+    event MetadataUpdate(uint256 _tokenId);
+
+    /// @dev This event emits when the metadata of a range of tokens is changed.
+    /// So that the third-party platforms such as NFT market could
+    /// timely update the images and related attributes of the NFTs.
+    event BatchMetadataUpdate(uint256 _fromTokenId, uint256 _toTokenId);
 }
 
-// File: @openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol
+// File: @openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol
 
 
-// OpenZeppelin Contracts (last updated v5.1.0) (token/ERC721/extensions/ERC721Enumerable.sol)
+// OpenZeppelin Contracts (last updated v5.1.0) (token/ERC721/extensions/ERC721URIStorage.sol)
 
 pragma solidity ^0.8.20;
 
 
 
 
+
 /**
- * @dev This implements an optional extension of {ERC721} defined in the ERC that adds enumerability
- * of all the token ids in the contract as well as all token ids owned by each account.
- *
- * CAUTION: {ERC721} extensions that implement custom `balanceOf` logic, such as {ERC721Consecutive},
- * interfere with enumerability and should not be used together with {ERC721Enumerable}.
+ * @dev ERC-721 token with storage based token URI management.
  */
-abstract contract ERC721Enumerable is ERC721, IERC721Enumerable {
-    mapping(address owner => mapping(uint256 index => uint256)) private _ownedTokens;
-    mapping(uint256 tokenId => uint256) private _ownedTokensIndex;
+abstract contract ERC721URIStorage is IERC4906, ERC721 {
+    using Strings for uint256;
 
-    uint256[] private _allTokens;
-    mapping(uint256 tokenId => uint256) private _allTokensIndex;
+    // Interface ID as defined in ERC-4906. This does not correspond to a traditional interface ID as ERC-4906 only
+    // defines events and does not include any external function.
+    bytes4 private constant ERC4906_INTERFACE_ID = bytes4(0x49064906);
+
+    // Optional mapping for token URIs
+    mapping(uint256 tokenId => string) private _tokenURIs;
 
     /**
-     * @dev An `owner`'s token query was out of bounds for `index`.
+     * @dev See {IERC165-supportsInterface}
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, IERC165) returns (bool) {
+        return interfaceId == ERC4906_INTERFACE_ID || super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        _requireOwned(tokenId);
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+        // If both are set, concatenate the baseURI and tokenURI (via string.concat).
+        if (bytes(_tokenURI).length > 0) {
+            return string.concat(base, _tokenURI);
+        }
+
+        return super.tokenURI(tokenId);
+    }
+
+    /**
+     * @dev Sets `_tokenURI` as the tokenURI of `tokenId`.
      *
-     * NOTE: The owner being `address(0)` indicates a global out of bounds index.
+     * Emits {MetadataUpdate}.
      */
-    error ERC721OutOfBoundsIndex(address owner, uint256 index);
-
-    /**
-     * @dev Batch mint is not allowed.
-     */
-    error ERC721EnumerableForbiddenBatchMint();
-
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
-    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165, ERC721) returns (bool) {
-        return interfaceId == type(IERC721Enumerable).interfaceId || super.supportsInterface(interfaceId);
-    }
-
-    /**
-     * @dev See {IERC721Enumerable-tokenOfOwnerByIndex}.
-     */
-    function tokenOfOwnerByIndex(address owner, uint256 index) public view virtual returns (uint256) {
-        if (index >= balanceOf(owner)) {
-            revert ERC721OutOfBoundsIndex(owner, index);
-        }
-        return _ownedTokens[owner][index];
-    }
-
-    /**
-     * @dev See {IERC721Enumerable-totalSupply}.
-     */
-    function totalSupply() public view virtual returns (uint256) {
-        return _allTokens.length;
-    }
-
-    /**
-     * @dev See {IERC721Enumerable-tokenByIndex}.
-     */
-    function tokenByIndex(uint256 index) public view virtual returns (uint256) {
-        if (index >= totalSupply()) {
-            revert ERC721OutOfBoundsIndex(address(0), index);
-        }
-        return _allTokens[index];
-    }
-
-    /**
-     * @dev See {ERC721-_update}.
-     */
-    function _update(address to, uint256 tokenId, address auth) internal virtual override returns (address) {
-        address previousOwner = super._update(to, tokenId, auth);
-
-        if (previousOwner == address(0)) {
-            _addTokenToAllTokensEnumeration(tokenId);
-        } else if (previousOwner != to) {
-            _removeTokenFromOwnerEnumeration(previousOwner, tokenId);
-        }
-        if (to == address(0)) {
-            _removeTokenFromAllTokensEnumeration(tokenId);
-        } else if (previousOwner != to) {
-            _addTokenToOwnerEnumeration(to, tokenId);
-        }
-
-        return previousOwner;
-    }
-
-    /**
-     * @dev Private function to add a token to this extension's ownership-tracking data structures.
-     * @param to address representing the new owner of the given token ID
-     * @param tokenId uint256 ID of the token to be added to the tokens list of the given address
-     */
-    function _addTokenToOwnerEnumeration(address to, uint256 tokenId) private {
-        uint256 length = balanceOf(to) - 1;
-        _ownedTokens[to][length] = tokenId;
-        _ownedTokensIndex[tokenId] = length;
-    }
-
-    /**
-     * @dev Private function to add a token to this extension's token tracking data structures.
-     * @param tokenId uint256 ID of the token to be added to the tokens list
-     */
-    function _addTokenToAllTokensEnumeration(uint256 tokenId) private {
-        _allTokensIndex[tokenId] = _allTokens.length;
-        _allTokens.push(tokenId);
-    }
-
-    /**
-     * @dev Private function to remove a token from this extension's ownership-tracking data structures. Note that
-     * while the token is not assigned a new owner, the `_ownedTokensIndex` mapping is _not_ updated: this allows for
-     * gas optimizations e.g. when performing a transfer operation (avoiding double writes).
-     * This has O(1) time complexity, but alters the order of the _ownedTokens array.
-     * @param from address representing the previous owner of the given token ID
-     * @param tokenId uint256 ID of the token to be removed from the tokens list of the given address
-     */
-    function _removeTokenFromOwnerEnumeration(address from, uint256 tokenId) private {
-        // To prevent a gap in from's tokens array, we store the last token in the index of the token to delete, and
-        // then delete the last slot (swap and pop).
-
-        uint256 lastTokenIndex = balanceOf(from);
-        uint256 tokenIndex = _ownedTokensIndex[tokenId];
-
-        mapping(uint256 index => uint256) storage _ownedTokensByOwner = _ownedTokens[from];
-
-        // When the token to delete is the last token, the swap operation is unnecessary
-        if (tokenIndex != lastTokenIndex) {
-            uint256 lastTokenId = _ownedTokensByOwner[lastTokenIndex];
-
-            _ownedTokensByOwner[tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
-            _ownedTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
-        }
-
-        // This also deletes the contents at the last position of the array
-        delete _ownedTokensIndex[tokenId];
-        delete _ownedTokensByOwner[lastTokenIndex];
-    }
-
-    /**
-     * @dev Private function to remove a token from this extension's token tracking data structures.
-     * This has O(1) time complexity, but alters the order of the _allTokens array.
-     * @param tokenId uint256 ID of the token to be removed from the tokens list
-     */
-    function _removeTokenFromAllTokensEnumeration(uint256 tokenId) private {
-        // To prevent a gap in the tokens array, we store the last token in the index of the token to delete, and
-        // then delete the last slot (swap and pop).
-
-        uint256 lastTokenIndex = _allTokens.length - 1;
-        uint256 tokenIndex = _allTokensIndex[tokenId];
-
-        // When the token to delete is the last token, the swap operation is unnecessary. However, since this occurs so
-        // rarely (when the last minted token is burnt) that we still do the swap here to avoid the gas cost of adding
-        // an 'if' statement (like in _removeTokenFromOwnerEnumeration)
-        uint256 lastTokenId = _allTokens[lastTokenIndex];
-
-        _allTokens[tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
-        _allTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
-
-        // This also deletes the contents at the last position of the array
-        delete _allTokensIndex[tokenId];
-        _allTokens.pop();
-    }
-
-    /**
-     * See {ERC721-_increaseBalance}. We need that to account tokens that were minted in batch
-     */
-    function _increaseBalance(address account, uint128 amount) internal virtual override {
-        if (amount > 0) {
-            revert ERC721EnumerableForbiddenBatchMint();
-        }
-        super._increaseBalance(account, amount);
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        _tokenURIs[tokenId] = _tokenURI;
+        emit MetadataUpdate(tokenId);
     }
 }
 
@@ -3370,49 +3264,20 @@ abstract contract Ownable is Context {
 // File: educateth_NFT_DLGT.sol
 
 
-// Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.22;
 
-contract DLGT_NFT is ERC721, ERC721Enumerable, Ownable {
-    uint256 private _nextTokenId;
+contract CypherpunkNFTV4 is ERC721URIStorage, Ownable {
+    uint256 public tokenCounter;
 
-    constructor(address initialOwner)
-        ERC721("DLGT_NFT", "DLGT")
-        Ownable(initialOwner)
-    {}
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://gateway.lighthouse.storage/ipfs/bafkreicegycumgqv3o35eb5yaghfyg3aekip4444zzqudkhzynajpzp3ji";
+    constructor(address initialOwner) ERC721("Educateth_DLGT_3.0", "DG") Ownable(initialOwner) {
+        tokenCounter = 0;
     }
 
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _update(address to, uint256 tokenId, address auth)
-        internal
-        override(ERC721, ERC721Enumerable)
-        returns (address)
-    {
-        return super._update(to, tokenId, auth);
-    }
-
-    function _increaseBalance(address account, uint128 value)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._increaseBalance(account, value);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+    function createNFT() public onlyOwner returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, "https://gateway.lighthouse.storage/ipfs/bafkreiees63v6u2uaibhaqy6el2nvagmkfnezniyaqm2z5aseeoqdsi63y");
+        tokenCounter += 1;
+        return newTokenId;
     }
 }
